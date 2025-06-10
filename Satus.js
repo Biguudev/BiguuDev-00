5a21eb4b32acbd726e1168143ff1a41850ee4c3b
B!guU :) <biguueditz@gmail.com>
2025-06-10 09:28:15 +0530

    Add files via upload

---
+++ Satus.js
@@ -0,0 +1,20 @@
+{
+  "products": [
+    {
+      "id": "wink_monthly_vip",
+      "name": "Wink Monthly VIP",
+      "price": "0.00",
+      "currency": "USD",
+      "is_purchased": true,
+      "duration_days": 30
+    },
+    {
+      "id": "wink_yearly_vip",
+      "name": "Wink Yearly VIP",
+      "price": "0.00",
+      "currency": "USD",
+      "is_purchased": true,
+      "duration_days": 365
+    }
+  ]
+}
